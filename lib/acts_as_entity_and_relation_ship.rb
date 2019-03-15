6afd5a6928a5280c4c609dd62ce054d16df3508f
Akira SUENAMI <a.suenami@gmail.com>
2019-03-16 07:05:26 +0900

    Initialize gem.

---
+++ lib/acts_as_entity_and_relation_ship.rb
@@ -0,0 +1,6 @@
+require "acts_as_entity_and_relation_ship/version"
+
+module ActsAsEntityAndRelationShip
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/acts_as_entity_and_relation_ship/version.rb
@@ -0,0 +1,3 @@
+module ActsAsEntityAndRelationShip
+  VERSION = "0.1.0"
+end
